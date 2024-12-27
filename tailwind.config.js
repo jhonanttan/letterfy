2f22bc7157989682de9eb83be6c194147c4d312b
Jhonanttan <jhonanttan1@gmail.com>
2024-12-27 11:05:16 -0300

    Tela de Album e Musica

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fundoGold: '#F8D092',
+        fundoVerde: '#38A48C',
+        fundoCiano: '#159097',
+        fundoAzul: '#1A5678',
+        fundoRoxo: '#25245E',
+        fundoDeepRoxo: '#571C56',
+      },
+    },
   },
   plugins: [],
 }
